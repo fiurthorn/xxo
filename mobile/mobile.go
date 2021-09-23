3549b2369bf3d3358340931642c03c81f24f0390
Weinmann <s.weinmann@intex-online.de>
2021-09-23 21:34:00 +0200

    remove title on android

--- mobile/mobile.go
+++ mobile/mobile.go
@@ -2,13 +2,10 @@ package mobile
 
 import (
 	"github.com/fiurthorn/xxo"
-	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/mobile"
 )
 
-// /fiurthorn/xxo/ebitenmobileview/ $ EbitenView
 func init() {
-	ebiten.SetWindowTitle("XXO")
 	g := xxo.NewGame(xxo.NewInput(), xxo.NewBoard())
 	mobile.SetGame(g)
 }
